419b8a54fcc99b9e8a904b467d14ef0236badc5e
jeyaragu <jeyaragus@gmail.com>
2025-12-30 16:04:57 +0530

    Updated files

--- node-js/create-server/app.js
+++ node-js/create-server/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParse = require('body-parser');
-const routes = require('./routes/assets-routes')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/assets-routes");
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server started with port', 3000)
+  console.log("Server started with port", 3000);
 });
 
-app.use(bodyParse.json());
-app.use('/api', routes);
+app.use(bodyParser.json());
+app.use("/api", routes);
--- node-js/create-server/controller/assets-controller.js
+++ node-js/create-server/controller/assets-controller.js
@@ -1,57 +1,57 @@
-const assetService = require('../service/asset-service');
-const { generateErrorResponse } = require('../utils/errorMessageHandler');
+const assetService = require("../service/asset-service");
+const { generateErrorResponse } = require("../utils/errorMessageHandler");
 
 // Create Asset
 function createAsset(req, res) {
-    try {
-        console.log('-Body', req.body)
-        assetService.upsertAsset(req.body);
-        res.status(201).send({
-            message: "Assets upserted successfully"
-        });
-    } catch (error) {
-        res.status(error.status || 500).send(generateErrorResponse(error));
-    }
-};
+  try {
+    console.log("-Body", req.body);
+    assetService.upsertAsset(req.body);
+    res.status(201).send({
+      message: "Assets upserted successfully",
+    });
+  } catch (error) {
+    res.status(error.status || 500).send(generateErrorResponse(error));
+  }
+}
 
-// Get All assets 
+// Get All assets
 function getAllAsset(req, res) {
-    try {
-        return res.status(200).send({
-            assets: assetService.getAllAssets(),
-            message: 'Success'
-        })
-    } catch (error) {
-         res.status(error.status).send(generateErrorResponse(error));
-    }
-};
+  try {
+    return res.status(200).send({
+      assets: assetService.getAllAssets(),
+      message: "Success",
+    });
+  } catch (error) {
+    res.status(error.status).send(generateErrorResponse(error));
+  }
+}
 
 // Update asset details by asset id
 function updateAssetById(req, res) {
-    try {
-        const { id } = req.params;
-        const assetInfo = req.body;
-        assetService.upsertAsset({ ...assetInfo, id })
-        res.status(204).send();
-    } catch (error) {
-         res.status(error.status || 500).send(generateErrorResponse(error));
-    }
-};
+  try {
+    const { id } = req.params;
+    const assetInfo = req.body;
+    assetService.upsertAsset({ ...assetInfo, id });
+    res.status(204).send();
+  } catch (error) {
+    res.status(error.status || 500).send(generateErrorResponse(error));
+  }
+}
 
 // Delete asset by asset id
 function deleteAsset(req, res) {
-    try {
-        const { id } = req.params;
-        assetService.removeAsset(id);
-        res.status(204).send();
-    } catch (error) {
-        res.status(error.status || 500).send(generateErrorResponse(error));
-    }
-};
+  try {
+    const { id } = req.params;
+    assetService.removeAsset(id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(error.status || 500).send(generateErrorResponse(error));
+  }
+}
 
 module.exports = {
-    createAsset,
-    getAllAsset,
-    updateAssetById,
-    deleteAsset
+  createAsset,
+  getAllAsset,
+  updateAssetById,
+  deleteAsset,
 };
